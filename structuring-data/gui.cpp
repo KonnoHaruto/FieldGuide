1cddab91eb4cc0e41dc1a7846936171656da6946
harukon <89727720+KonnoHaruto@users.noreply.github.com>
2026-04-29 17:42:35 +0800

    17:42

--- structuring-data/gui.cpp
+++ structuring-data/gui.cpp
@@ -1,6 +1,25 @@
 #include "splashkit.h"
 
+struct target_data
+{
+    double x;
+    double y;
+    double radius;
+};
+
 int main()
 {
-    
+    open_window("Reaction Timer", 800, 600);
+
+    target_data target;
+
+    while (!quit_requested())
+    {
+        process_events();
+
+        clear_screen(COLOR_WHITE);
+        refresh_screen();
+    }
+
+    return 0;
 }
